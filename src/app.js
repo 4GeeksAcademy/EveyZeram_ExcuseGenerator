2a7609ab69c067cde640ffb44bbc5f69c34875a5
Evelyn <130219194+EveyZeram@users.noreply.github.com>
2023-10-13 18:26:21 +0000

    eveygm mejoras excusegenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function onLoad() {
+  let who = [`Mi perro`, `Mi pareja`, `My tatarabuelo`, `Mi casero`, `Mi ex`];
+  let what = [`comió`, `cogió`, `robó`, `machacó`, `hizo papiroflexia con`];
+  let thing = [`deberes`, `trabajo`, `documentos`];
+  let when = [`hoy`, `hace 84 años`, `mientras comía`, `mientras yo dormía`];
+  var excusa = document.getElementById("excuse");
+  excusa.innerHTML = excuseGenerator(who, what, thing, when);
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excuseGenerator(param1, param2, param3, param4) {
+  let aWho = Math.floor(Math.random() * param1.length);
+  let aWhat = Math.floor(Math.random() * param2.length);
+  let aThing = Math.floor(Math.random() * param3.length);
+  let aWhen = Math.floor(Math.random() * param4.length);
+  return (
+    param1[aWho] +
+    ` ` +
+    param2[aWhat] +
+    ` ` +
+    param3[aThing] +
+    ` ` +
+    param4[aWhen]
+  );
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = onLoad;
